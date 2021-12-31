71b005482958bda2b523f9124d8e49a4d5bb1859
SlimeVidas <dahan.isaac@gmail.com>
2021-12-31 15:58:41 +0200

    fetchSongs-function-in-App.js

--- dynamo_README.md
+++ dynamo_README.md
@@ -20,3 +20,57 @@ type Song @model {
 ### `git add , commit , push`
 
 ### `amplify push`
+
+## Create fetchSongs function in App.js
+
+```jsx
+import React, { useEffect, useState } from 'react'
+import Amplify, { API, graphqlOperation } from 'aws-amplify'
+import { listSongs } from './graphql/queries'
+import './App.css'
+
+import { withAuthenticator , AmplifySignOut } from '@aws-amplify/ui-react'
+import awsExports from "./aws-exports";
+Amplify.configure(awsExports);
+
+
+
+
+ function App() {
+
+useEffect(() => {
+    fetchSongs()
+    return () => {
+        "Songs"
+    }
+}, [])
+
+const fetchSongs = async()=>{
+    try {
+        const songsData = await API.graphql(graphqlOperation(listSongs))
+        console.log('songsData' , songsData)
+        const songsList = songsData.data.listSongs.items
+        console.log("songsList" , songsList)  
+    } catch (error) {
+        console.log(error)
+    }
+    
+}
+
+
+
+
+    return (
+        <div className='App'>
+            <header className="App-header">
+            <AmplifySignOut />
+            <h2>My App Content</h2>
+            </header>
+        </div>
+    )
+}
+
+
+export default withAuthenticator(App)
+
+```
--- src/App.js
+++ src/App.js
@@ -1,83 +1,48 @@
-/* src/App.js */
 import React, { useEffect, useState } from 'react'
 import Amplify, { API, graphqlOperation } from 'aws-amplify'
-import { createTodo } from './graphql/mutations'
-import { listTodos } from './graphql/queries'
-import { withAuthenticator , AmplifySignOut } from '@aws-amplify/ui-react'
+import { listSongs } from './graphql/queries'
+import './App.css'
 
+import { withAuthenticator , AmplifySignOut } from '@aws-amplify/ui-react'
 import awsExports from "./aws-exports";
 Amplify.configure(awsExports);
 
-const initialState = { name: '', description: '' }
 
-const App = () => {
-  const [formState, setFormState] = useState(initialState)
-  const [todos, setTodos] = useState([])
 
-  useEffect(() => {
-    fetchTodos()
-  }, [])
 
-  function setInput(key, value) {
-    setFormState({ ...formState, [key]: value })
-  }
+ function App() {
 
-  async function fetchTodos() {
-    try {
-      const todoData = await API.graphql(graphqlOperation(listTodos))
-      const todos = todoData.data.listTodos.items
-      setTodos(todos)
-    } catch (err) { console.log('error fetching todos') }
-  }
-
-  async function addTodo() {
-    try {
-      if (!formState.name || !formState.description) return
-      const todo = { ...formState }
-      setTodos([...todos, todo])
-      setFormState(initialState)
-      await API.graphql(graphqlOperation(createTodo, {input: todo}))
-    } catch (err) {
-      console.log('error creating todo:', err)
+useEffect(() => {
+    fetchSongs()
+    return () => {
+        "Songs"
     }
-  }
+}, [])
 
-  return (
-    <div style={styles.container}>
-      <h2>Amplify Todos</h2>
-      <input
-        onChange={event => setInput('name', event.target.value)}
-        style={styles.input}
-        value={formState.name}
-        placeholder="Name"
-      />
-      <input
-        onChange={event => setInput('description', event.target.value)}
-        style={styles.input}
-        value={formState.description}
-        placeholder="Description"
-      />
-      <button style={styles.button} onClick={addTodo}>Create Todo</button>
-      {
-        todos.map((todo, index) => (
-          <div key={todo.id ? todo.id : index} style={styles.todo}>
-            <p style={styles.todoName}>{todo.name}</p>
-            <p style={styles.todoDescription}>{todo.description}</p>
-          </div>
-        ))
-      }
-      <AmplifySignOut/>
-    </div>
-  )
+const fetchSongs = async()=>{
+    try {
+        const songsData = await API.graphql(graphqlOperation(listSongs))
+        console.log('songsData' , songsData)
+        const songsList = songsData.data.listSongs.items
+        console.log("songsList" , songsList)  
+    } catch (error) {
+        console.log(error)
+    }
+    
 }
 
-const styles = {
-  container: { width: 400, margin: '0 auto', display: 'flex', flexDirection: 'column', justifyContent: 'center', padding: 20 },
-  todo: {  marginBottom: 15 },
-  input: { border: 'none', backgroundColor: '#ddd', marginBottom: 10, padding: 8, fontSize: 18 },
-  todoName: { fontSize: 20, fontWeight: 'bold' },
-  todoDescription: { marginBottom: 0 },
-  button: { backgroundColor: 'black', color: 'white', outline: 'none', fontSize: 18, padding: '12px 0px' }
+
+
+
+    return (
+        <div className='App'>
+            <header className="App-header">
+            <AmplifySignOut />
+            <h2>My App Content</h2>
+            </header>
+        </div>
+    )
 }
+
 
 export default withAuthenticator(App)
